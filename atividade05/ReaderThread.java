be52a53d61da5a39bb00c8d924a59e76ebde14b4
limaalan <alanlimamarques6@gmail.com>
2024-10-16 23:04:54 -0300

    ex05

--- atividade05/ReaderThread.java
+++ atividade05/ReaderThread.java
@@ -1,25 +1,42 @@
 
+import java.util.ArrayList;
 import java.util.Scanner;
-import javax.swing.JOptionPane;
 
 public class ReaderThread {
+    private static volatile ArrayList<Integer> readValues = new ArrayList<>();
+
     public static void main(String[] args) {
-        new Thread(()->{
+        Thread readerThread = new Thread(()->{
+            System.out.println("\n Entre com valores inteiros, digite -1 para sair.");
             Scanner reader = new Scanner(System.in);
-            String line ="";
+            Integer line =0;
             
-            while ( Integer.valueOf(line)!= null ){
+            while ( line != null && line!= -1){
                 System.out.println("Digite :");
-                line = reader.nextLine();
-
+                readValues.add(line);
+                line = Integer.valueOf(reader.nextLine());
+                
                 System.out.println("Lido:" + line );
-                System.out.printf("lido : %s\n" , line);
             }
-
+            reader.close();
             
-            String texto = JOptionPane.showInputDialog(null , "Digite : ");
-            JOptionPane.showMessageDialog(null, texto);
-        }).start();
+        });
+        
+        System.out.println("Iniciando thread de leitura...");
+        readerThread.start();
+        try {
+            readerThread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Thread de leitura finalizada.");
+
+        Integer sum = 0;
+        for (Integer elem : readValues) {
+            sum += elem;
+        }
+
+        System.out.println("Soma é : " + sum);
     }
 
 }
